c0a3618f3ada82c073d2867de6f5a89642961419
Okpah Evans Chukwunnaemeka <105133449+OkpahEvans@users.noreply.github.com>
2023-02-04 15:23:09 -0800

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
 #include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* main_h */
+#endif /* MAIN_H */
